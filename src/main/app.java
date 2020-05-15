06e7c398a83fa9a424a514e64306f57ca8bc5d57
Daniel <Daniel@DESKTOP-L1O2SVR>
2020-05-15 11:30:55 -0500

    Added files

---
+++ src/main/app.java
@@ -0,0 +1,16 @@
+package main;
+
+import model.employee;
+
+public class app {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Bitches");
+        employee myEmployee = new employee();
+        myEmployee.setEmployeeID(10);
+        myEmployee.setSalary(100000);
+        myEmployee.setEmployeeName("Daniel");
+        System.out.println(myEmployee.getEmployeeID() + " " + myEmployee.getEmployeeName() +" makes " + myEmployee.getSalary() + " dollars a year");
+
+    }
+}
---
+++ src/model/employee.java
@@ -0,0 +1,34 @@
+package model;
+import java.util.*;
+import java.io.*;
+import java.lang.String;
+
+public class employee {
+    private int employeeID;
+    private String employeeName;
+    private double salary;
+
+    public int getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(int employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getEmployeeName() {
+        return employeeName;
+    }
+
+    public void setEmployeeName(String employeeName) {
+        this.employeeName = employeeName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
